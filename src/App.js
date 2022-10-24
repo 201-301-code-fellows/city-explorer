dc528dabfbb95d171c3f3c47fbe2a9ef69c4437c
Lewis Benson <tm.lewis.benson@gmail.com>
2022-10-24 17:12:11 -0400

    change to class component import Main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+import React, { Component } from 'react'
+import Main from './components/Main'
+export default class App extends Component {
+  render() {
+    return (
+      <Main />
+    )
+  }
 }
 
-export default App;
