d9f0237b38b242989e96b885285d2a8c1dcea307
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-10-26 16:19:40 -0400

    bug fix for new api; change url to local for dev

--- src/components/WeatherMessage.js
+++ src/components/WeatherMessage.js
@@ -3,7 +3,7 @@ import styles from './WeatherMessage.module.css'
 
 export default class WeatherMessage extends Component {
   render() {
-
+    console.log(this.props)
     return (
       this.props.error ? <h2>No weather data found</h2> :
         < section >
@@ -11,7 +11,7 @@ export default class WeatherMessage extends Component {
             { }
           </h2><ul>
             {this.props.weatherData ?
-              this.props.weatherData.description.map((item, index) => {
+              this.props.weatherData.map((item, index) => {
                 return <li key={index}>{item.date} {item.description}</li>
               }) : <></>
             }</ul>
